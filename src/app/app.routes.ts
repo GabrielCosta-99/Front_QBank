b96b5d6af6bb1a1d2fff1f1ad54977e98cc4a095
luiz gustavo domingues lacerda <lacerdadomingues1@gmail.com>
2024-11-25 19:17:55 -0300

    +

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Importação dos componentes
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ClientsComponent } from './pages/clients/clients.component';
+
+export const routes: Routes = [
+  // Rota para o Login
+  { path: 'login', component: LoginComponent },
+
+  // Rota para a Home
+  { path: 'home', component: HomeComponent },
+
+  // Rota para a página de Clients
+  { path: 'clients', component: ClientsComponent },
+
+  // Redireciona para a rota 'login' caso a URL seja acessada com um caminho vazio
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+
+  // Rota curinga para redirecionar em caso de URL inválida
+  { path: '**', redirectTo: '/login' }
+];
--- src/app/services/app.module.ts
+++ src/app/services/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'; // Adicione essa linha
+import { HttpClientModule } from '@angular/common/http'; // Necessário para fazer requisições HTTP
+import { FormsModule } from '@angular/forms'; // Necessário para trabalhar com formulários
 import { AppComponent } from './app.component';
 
+// Se você tem um serviço AuthService
+import { AuthService } from './services/auth.service'; // Ajuste o caminho conforme necessário
+import { HomeComponent } from './pages/home/home.component'; // Importação do seu componente
+
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, FormsModule], // Adicionando FormsModule
-    providers: [],
-    bootstrap: [AppComponent],
-  })
-  export class AppModule {}
+  declarations: [
+    AppComponent,
+    HomeComponent, // Certifique-se de declarar todos os componentes necessários aqui
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule, // Para fazer requisições HTTP
+    FormsModule, // Para usar formulários no Angular
+  ],
+  providers: [AuthService], // Registre seu AuthService aqui
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
