287f68c6f737708be04fc47997f7c195f9d464d7
Gabriel costa <128004263+GabrielCosta-99@users.noreply.github.com>
2024-11-26 11:32:53 -0300

    mudanças layout home e troca de rotas

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [CommonModule],
   template: `
   <div class="container">
     <header>
